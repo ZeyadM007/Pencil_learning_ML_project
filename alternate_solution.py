bd14cdc780d0d41a2374c95f79c7cc657d5d3813
ZeyadM007 <61668904+ZeyadM007@users.noreply.github.com>
2021-02-06 11:24:07 -0500

    Update alternate_solution.py

--- alternate_solution.py
+++ alternate_solution.py
@@ -16,8 +16,10 @@ file_path = get_file('shakespear2.txt', 'https://homl.info/shakespeare')
 with open(file_path) as f:
     data = f.read()
 
+#Spliting the lines of the text into a list
 corpus = data.lower().split('\n')
 
+#Tokenizer used to assign a value to each word
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(corpus)
 total_words = len(tokenizer.word_index) + 1
@@ -27,24 +29,28 @@ print(total_words)
 
 input_sequences = []
 
+#Tokenizer used to convert the texts into sequences using the values the tokenizer assigned to each word
 for line in corpus:
     token_list = tokenizer.texts_to_sequences([line])[0]
     for i in range(1, len(token_list)):
         n_gram_sequence = token_list[:i+1]
         input_sequences.append(n_gram_sequence)
 
+#Taking the last word to make it the target for training
 xs = []
 ys = []
 for line in input_sequences:
     xs.append(line[:-1])
     ys.append(line[-1])
 
+#Padded the input_sequences to make them all uniform. Then took all the columns of the training except the last one.
 max_sequence_len = max([len(x) for x in input_sequences])
 alternate_training = pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre')
 alternate_training = alternate_training[:,:-1]
 ys = np.array(ys)
 
-
+#This model is a Sequential model unlike the main_model. This model uses custom made Embeddings to create a 64 dimensional vector. The similarity in both models is
+# they both run with sparse_categorical loss function.
 model_alt = Sequential()
 model_alt.add(Embedding(12633, 64, input_length=max_sequence_len-1))
 model_alt.add(Conv1D(128, 5, activation='relu'))
