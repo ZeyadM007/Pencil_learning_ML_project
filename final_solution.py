abc15ac6b8da9b81cd3279a1015c7dd52ee79691
ZeyadM007 <61668904+ZeyadM007@users.noreply.github.com>
2021-02-06 11:24:24 -0500

    Update final_solution.py

--- final_solution.py
+++ final_solution.py
@@ -16,8 +16,10 @@ file_path = get_file('shakespear2.txt', 'https://homl.info/shakespeare')
 with open(file_path) as f:
     data = f.read()
 
+#Spliting the lines of the text into a list
 corpus = data.lower().split('\n')
 
+#Tokenizer used to assign a value to each word
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(corpus)
 total_words = len(tokenizer.word_index) + 1
@@ -27,22 +29,27 @@ print(total_words)
 
 input_sequences = []
 
+
+#Tokenizer used to convert the texts into sequences using the values the tokenizer assigned to each word
 for line in corpus:
     token_list = tokenizer.texts_to_sequences([line])[0]
     for i in range(1, len(token_list)):
         n_gram_sequence = token_list[:i+1]
         input_sequences.append(n_gram_sequence)
 
+ #Taking the last word to make it the target for training
 xs = []
 ys = []
 for line in input_sequences:
     xs.append(line[:-1])
     ys.append(line[-1])
 
+#COnverting the training back to words because the nnlm model and the universal sentence encoder take in words as the input.
 training = tokenizer.sequences_to_texts(xs)
 training = np.array(training)
 ys = np.array(ys)
 
+#Importing universal sentence encoder and nnlm model from TF Hub using tensorflow_hub
 universal_sentence_encoder = KerasLayer("https://tfhub.dev/google/universal-sentence-encoder/4", input_shape=[], output_shape=[512],dtype=tf.string)
 nnlm_dim50 = KerasLayer("https://tfhub.dev/google/nnlm-en-dim50/2", trainable=False, input_shape=[], output_shape=[50],dtype=tf.string)
 
@@ -58,14 +65,16 @@ merged = Concatenate()([model_1.output, model_2.output])
 merged = Dense(32, activation='relu')(merged)
 merged = Dense(12633, activation='softmax')(merged)
 
+#Model is a combination of 2 Sequential models each with their respective weights. The models are then combined at merged variable to produce 1 final outcome.
 model = Model(inputs=[model_1.input, model_2.input], outputs=[merged])
 
+#Used sparse_categorical_crossentropy as a loss function and used Adam as an optimizer
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 history = model.fit([training, training], ys, epochs=10, verbose=1)
 
 
-
+#This function outputs the next 100 predicted words
 def predict_sentence(word):
     for _ in range(100):
         input_for_prediction = np.asarray([word])
